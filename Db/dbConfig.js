9bda0f0c468310016200f6d261c4c3f6ce436449
christian orlando <christianorlando@pursuit.org>
2023-05-22 09:42:32 -0400

    dbconfig

--- Db/dbConfig.js
+++ Db/dbConfig.js
@@ -5,7 +5,7 @@ const { DATABASE_URL } = process.env
 
 
 const cn =  DATABASE_URL ?
- { connectionString: DATABASE_URL, max:30,}
+ { connectionString: DATABASE_URL, max:30}
 :{
     host: process.env.PG_HOST,
     port: process.env.PG_PORT,
